2546ddfa03571917d2c74e37d82cbc2ec90b1043
giginet <giginet.net@gmail.com>
2016-08-28 18:03:55 +0900

    Compatible with Python 2.7

--- runtests.py
+++ runtests.py
@@ -8,7 +8,11 @@ from django.test.utils import get_runner
 
 
 def run_tests():
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+    base_dir = os.path.dirname(__file__)
+    sys.path.insert(0, os.path.join(base_dir, 'slack_invitation'))
+    sys.path.insert(0, os.path.join(base_dir, 'tests'))
+
     django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
