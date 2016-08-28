09c20025b267c61239d1464ec53f1c973ddf879e
giginet <giginet.net@gmail.com>
2016-08-28 15:31:31 +0900

    Initial Commit

---
+++ slack-invitation/signals.py
@@ -0,0 +1,1 @@
+from registration.signals import user_activated
---
+++ slack-invitation/slack.py
@@ -0,0 +1,33 @@
+import request
+from .compatibility import urljoin
+
+
+class SlackInvitationException(BaseException):
+    pass
+
+
+class SlackInvitationClient(object):
+    BASE_URL = 'https://{}.slack.com'
+    ENDPOINT = '/api/users.admin.invite'
+
+    def __init__(self, team, token):
+        self.token = token
+        self.team = team
+
+    @property
+    def base_url(self):
+        return self.BASE_URL.format(self.team)
+
+    def invite(self, email, active=True):
+        endpoint = urljoin(self.base_url, self.ENDPOINT)
+        r = request.post(endpoint, data={
+            'email': email,
+            'token': self.token,
+            'set_active': active
+        })
+        response_object = r.json()
+        if r.status_code == 200 and response_object['ok']:
+            return True
+        else:
+            raise SlackInvitationException(response_object['error'])
+
