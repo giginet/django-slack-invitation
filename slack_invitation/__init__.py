002e466123c1eb2dca0fd5c136ae1d1f9315f410
giginet <giginet.net@gmail.com>
2016-08-28 18:34:57 +0900

    Add slack_invitation to INSTALLED_APPS

--- slack_invitation/__init__.py
+++ slack_invitation/__init__.py
@@ -1,8 +1,7 @@
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from .signals import invite_to_slack
-
 def register_invite_to_slack():
+    from django.contrib.auth.models import User
+    from .signals import invite_to_slack
+    from django.db.models.signals import post_save
     post_save.connect(invite_to_slack, sender=User)
 
 
